cacfc1de40047b5e43e07a01ddee6836f3245611
Rup medhi <rupmmedhi@gmail.com>
2023-07-19 00:19:19 +0530

    cart

--- src/Components/Modal/Modal.js
+++ src/Components/Modal/Modal.js
@@ -1,8 +1,27 @@
 import React from 'react'
+import { TiDeleteOutline } from 'react-icons/ti';
+import './Modal.css'
 
 export default function Modal() {
   return (
-    <div>
+    <div className='main__container'>
+        <div className='icon'>
+             <TiDeleteOutline />
+        </div>
+     
+      <h2 className='modal__cart'>CART</h2>
+      <ul className='ul__list'>
+        <li>ITEM</li>
+        <li>PRICE</li>
+        <li>QUANTITY</li>
+      </ul>
+      <div className='total'>
+         <span>Total $ 9</span>
+      </div>
+     
+     <div className='purchaseBtn'>
+        <button className='btn'>PURCHASE</button>
+     </div>
       
     </div>
   )
